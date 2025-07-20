05b8e5ced27310e5290603d1110bfaa05c42b846
kuldeeppaikara <112508710+kuldeeppaikara@users.noreply.github.com>
2025-07-20 13:57:00 +0530

    storage-update-2

--- Front-end/src/components/Item.jsx
+++ Front-end/src/components/Item.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Item(props) {
   return (
     <div className="item w-64 hover:transform h-96 hover:scale-105 duration-600">
-      <Link to={`/product/${props.id}`}><img className="w-full h-[75%] object-cover object-top" onClick={window.scrollTo(0, 0)} src={`${import.meta.env.VITE_API_URL}${props.image}`} alt="" /></Link>
+      <Link to={`/product/${props.id}`}><img className="w-full h-[75%] object-cover object-top" onClick={window.scrollTo(0, 0)} src={props.image} alt="" /></Link>
       <p className="mt-6 mb-3 text-lg/tight font-medium">{props.name}</p>
       <div className="item-price  flex justify-between px-1 text-lg font-semibold ">
         <div className="new-price text-[#374151]">${props.new_price}</div>
